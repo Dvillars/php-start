012b4f675b1e172025e237771538838d4ca8c08d
Derek Villars <dv DerekVillars@yahoo.com>
2017-05-24 16:28:37 -0700

    add next

---
+++ buds.php
@@ -0,0 +1,17 @@
+<?php $friend_name = "Dritfy"; ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Greetings!</title>
+</head>
+<body>
+    <h1>Greetings From Afar</h1>
+    <p>Dear <?php echo $friend_name; ?>,</p>
+    <p>How are you? I hope that you are having a nice float. I'm vacationing in the mountains of Mt. Coronet while I learn great pokemon trainning pratices!</p>
+    <p><?php echo $friend_name; ?>, you would not believe how cold it is here. I should have gone to the Alolan islands instead. But I wanted to see dawn, so I've been havin' fun.</p>
+    <p>Looking forward to seeing you soon.</p>
+    <p>Cheers,</p>
+    <p>Derek</p>
+</body>
+</html>
--- index.php
+++ index.php
@@ -5,11 +5,13 @@
   <head>
     <meta charset="utf-8">
     <title>Hey whats up Php</title>
+    <link rel="stylesheet" href="/css/styles.css" type="text/css">
   </head>
   <body>
-    <h1>PHP headder whoot whoot</h1>
+    <h1>PHP Header Whoot Whoot</h1>
     <p>
       <?php echo $message; ?>
+      checkout your buds <a href="buds.php">click here</a>
     </p>
   </body>
 </html>
