6f9ce79f0132e1337e3ebbba76630a9d3a3992a7
Derek Villars <dv DerekVillars@yahoo.com>
2017-05-24 09:40:48 -0700

    started and understanding | basic html page

---
+++ tests.php
@@ -0,0 +1,15 @@
+<?php $message = "Hello PHP!"; ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Hey whats up Php</title>
+  </head>
+  <body>
+    <h1>PHP headder whoot whoot</h1>
+    <p>
+      <?php echo $message; ?>
+    </p>
+  </body>
+</html>
