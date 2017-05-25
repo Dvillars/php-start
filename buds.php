01a39bd469849ae96078ef869f87299b600d4dba
derek villars <derekvillars@yahoo.com>
2017-05-25 16:48:36 -0700

    add stuff

--- buds.php
+++ buds.php
@@ -1,4 +1,23 @@
-<?php $friend_name = "Dritfy"; ?>
+<?php
+    $friend_name_first = $_GET["recipient_first"];
+    $friend_name_last = $_GET["recipient_last"];
+    $friend_name = $friend_name_first . " " . $friend_name_last;
+    $my_name = $_GET["sender"];
+    $first_number = $_GET["first_number"];
+    $second_number = $_GET["second_number"];
+    function encode($first, $second)
+    {
+        $solution = $first + $second;
+        while ($solution > 6) {
+            $solution = $solution - 6;
+        }
+        if ($solution == 6) {
+            $solution = $solution - 1;
+        }
+        return $solution;
+    }
+    $solution = encode($first_number, $second_number);
+?>
 
 <!DOCTYPE html>
 <html>
@@ -7,11 +26,11 @@
 </head>
 <body>
     <h1>Greetings From Afar</h1>
-    <p>Dear <?php echo $friend_name; ?>,</p>
+    <p>Dear <strong><?php echo $friend_name ?></strong>,</p>
     <p>How are you? I hope that you are having a nice float. I'm vacationing in the mountains of Mt. Coronet while I learn great pokemon trainning pratices!</p>
-    <p><?php echo $friend_name; ?>, you would not believe how cold it is here. I should have gone to the Alolan islands instead. But I wanted to see dawn, so I've been havin' fun.</p>
-    <p>Looking forward to seeing you soon.</p>
+    <p><strong><?php echo $friend_name_first ?></strong>, you would not believe how cold it is here. I should have gone to the Alolan islands instead. But I wanted to see dawn, so I've been havin' fun.</p>
+    <p>Looking forward to seeing you soon. I need you on my team Asap I have a spot for you right now I have a team of <strong><?php echo $solution ?></strong>, so I need you buddy</p>
     <p>Cheers,</p>
-    <p>Derek</p>
+    <p><strong><?php echo $my_name ?></strong></p>
 </body>
 </html>
--- index.php
+++ index.php
@@ -5,13 +5,37 @@
   <head>
     <meta charset="utf-8">
     <title>Hey whats up Php</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/styles.css" type="text/css">
   </head>
   <body>
-    <h1>PHP Header Whoot Whoot</h1>
-    <p>
-      <?php echo $message; ?>
-      checkout your buds <a href="buds.php">click here</a>
-    </p>
+    <div class="container">
+        <h1>Add your name and a buddys to wright a message to your drifloon Whoot Whoot</h1>
+
+        <form action="buds.php">
+            <div class="form-group">
+                <label for="sender">Your name</label>
+                <input id="sender" name="sender" class="form-control" type="text">
+            </div>
+            <div class="form-group">
+                <label for="recipient_first">Your friend's first name</label>
+                <input id="recipient_first" name="recipient_first" class="form-control" type="text">
+            </div>
+            <div class="form-group">
+                <label for="recipient_last">Your friend's last name</label>
+                <input id="recipient_last" name="recipient_last" class="form-control" type="text">
+            </div>
+            <div class="form-group">
+                <label for="first_number">Your first number</label>
+                <input id="first_number" name="first_number" class="form-control" type="text">
+            </div>
+            <div class="form-group">
+                <label for="second_number">Your second number</label>
+                <input id="second_number" name="second_number" class="form-control" type="text">
+            </div>
+            <button type="submit" class="btn-success btn">Go!</button>
+        </form>
+
+    </div>
   </body>
 </html>
